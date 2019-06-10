6aa79ed45df9450b74a9bd2adef55960e379c4ec
Shengteng Ouyang <shengtengouyang@gmail.com>
2019-06-09 21:41:43 -0400

    add two modified class

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,19 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+
+    public void addFirst (T items){}
+
+    public void addLast(T item){}
+
+    public boolean isEmpty(){}
+
+    public int size(){}
+
+    public void printDeque(){}
+
+    public T removeFirst(){}
+
+    public T removeLast(){}
+
+    public T get(int index){}
+
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,3 +1,19 @@
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+
+    public void addFirst (T items){}
+
+    public void addLast(T item){}
+
+    public boolean isEmpty(){}
+
+    public int size(){}
+
+    public void printDeque(){}
+
+    public T removeFirst(){}
+
+    public T removeLast(){}
+
+    public T get(int index){}
 
 }
